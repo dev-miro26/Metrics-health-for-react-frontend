bf41e111b44c7acfb425d70c6485985738de850a
julianyeo <julianyeo26@gmail.com>
2023-03-17 03:12:12 +0800

    feat add component and hook

---
+++ src/components/BootstrapDialog.js
@@ -0,0 +1,11 @@
+import { styled } from "@mui/material/styles";
+import Dialog from "@mui/material/Dialog";
+
+export const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  "& .MuiDialogContent-root": {
+    padding: theme.spacing(3),
+  },
+  "& .MuiDialogActions-root": {
+    padding: theme.spacing(1),
+  },
+}));
