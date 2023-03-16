46afba8fff54787afe939f15c7d44290437f8155
bpolishevsk <bpolishevsk@gmail.com>
2023-03-16 21:52:19 +0200

    update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -16,13 +16,6 @@ const api = axios.create({
     Accept: "application/json",
   },
 });
-/*
-  NOTE: intercept any error responses from the api
- and check if the token is no longer valid.
- ie. Token has expired or user is no longer
- authenticated.
- logout the user if the token has expired
-*/
 
 // api interceptors
 
