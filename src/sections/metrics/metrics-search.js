3c3ea2f9057b00d96af5f0aeaecdb61b401b9883
julianyeo <julianyeo26@gmail.com>
2023-03-23 04:04:34 +0800

    feat total wages page

--- src/sections/metrics/metrics-search.js
+++ src/sections/metrics/metrics-search.js
@@ -1,10 +1,10 @@
 import MagnifyingGlassIcon from "@heroicons/react/24/solid/MagnifyingGlassIcon";
 import PropTypes from "prop-types";
 
-import { Card, InputAdornment, OutlinedInput, SvgIcon } from "@mui/material";
+import { InputAdornment, OutlinedInput, SvgIcon } from "@mui/material";
 
 export const MetricsSearch = ({ search, setSearch }) => (
-  <Card sx={{ p: 2 }}>
+  <div sx={{ p: 2 }}>
     <OutlinedInput
       fullWidth
       placeholder="Search metrics"
@@ -21,7 +21,7 @@ export const MetricsSearch = ({ search, setSearch }) => (
       }
       sx={{ maxWidth: 500 }}
     />
-  </Card>
+  </div>
 );
 MetricsSearch.prototype = {
   search: PropTypes.object.isRequired,
